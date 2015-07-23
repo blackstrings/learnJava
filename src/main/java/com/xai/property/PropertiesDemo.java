c4686f4bb131927e51c91da5e81e79092aa7cbec
Xai <xlao@menards.net>
2015-07-23 11:33:10 -0500

    added properties demo

---
+++ src/main/java/com/xai/property/PropertiesDemo.java
@@ -0,0 +1,38 @@
+package com.xai.property;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * how to grab key/value from properties file
+ */
+public class PropertiesDemo {
+
+	public static void main(String [] args){
+		
+		String propertyLocation = "src/main/java/com/xai/property/SomeData.properties";	//file location
+		Properties prop = new Properties();	//new properties object
+		
+		try {
+			//get the file into stream
+			FileInputStream fs = new FileInputStream(propertyLocation);
+			//load the file
+			prop.load(fs);													
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//get the values
+		String height = prop.getProperty("height");
+		
+		//set the value
+		int num = Integer.parseInt(height);
+	}
+	
+}
