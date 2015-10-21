c57a834692d69a44a349a69581b7d725bbc7f4e6
Xai <xlao@menards.net>
2015-10-21 10:49:46 -0500

    update test print num

--- src/main/java/com/xai/utils/Utils.java
+++ src/main/java/com/xai/utils/Utils.java
@@ -23,6 +23,44 @@ public class Utils {
 		
 		logger.info("" + Math.random());
 		
+		int sideLength = 8;
+		
+		for(int row=0; row<sideLength; row++){
+			
+			for(int col=0; col<sideLength; col++){
+				System.out.print("1");
+			}
+			
+			System.out.print("_");
+			
+			if(row == 0 || row == sideLength-1){
+				printFirstLastRow(sideLength);
+			}else{
+				printMiddle(sideLength);
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}
+	
+	public static void printFirstLastRow(int sideLength){
+		for(int i=0; i<sideLength; i++){
+			System.out.print("2");
+		}
+	}
+	
+	public static void printMiddle(int sideLength){
+		for(int i=0; i<sideLength; i++){
+			
+			if(i==0 || i== sideLength-1){
+				System.out.print("3");
+			}else{
+				System.out.print("0");
+			}
+	
+		}
 	}
 	
 	
