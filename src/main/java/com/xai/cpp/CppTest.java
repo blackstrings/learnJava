d978d8680b31db5084f62a5d44de0ce6fb3678b6
Xai <xlao@menards.net>
2016-01-23 10:46:07 -0600

    some updates

---
+++ src/main/java/com/xai/cpp/CppTest.java
@@ -0,0 +1,87 @@
+package com.xai.cpp;
+
+public class CppTest {
+
+	/*
+	
+	 // CPPLEARN2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Dog.h"
+#include <cstdlib>	//exist_success
+#include <iostream>	//cout
+
+using namespace std;
+
+
+//new class
+class Test(){
+	
+	public:
+		Test();
+		virtual ~Test();
+		void run();
+	private:
+		char names;
+}
+
+
+//implement
+Test::Test(){}
+Test::~Test(){}
+
+void Test::run(){
+	cout << "running";
+}
+
+
+//int main(int argc, char* argv[])
+int main()
+{
+	printf("Hello World!\n");
+	
+	//create new dog with default constuctor
+	Dog d;
+	//tell dog to bark
+	d.bark();
+
+
+	//new line
+	printf("\n");
+
+	
+		int var = 55;
+	char myArray[99];
+
+	char str[10] = "tommy";
+
+	//defining a pointer
+	char *a;
+	a = "my strings yep yep";
+	a = "my strings no no";
+
+
+	char *b = a;
+	b = "my b string";
+	
+	//cout << a;	// "my strings"
+	//cout << *a;	// 'm';
+
+	//cout << &a;		//address of pointer a
+
+	//manipulating the value through pointer
+	//char holder[99];
+	//strcpy(holder, a); 
+	//char *c = holder;
+	
+	cout << a << endl;
+	cout << b;
+
+	return EXIT_SUCCESS;
+}
+
+	  
+	 
+	 */
+}
