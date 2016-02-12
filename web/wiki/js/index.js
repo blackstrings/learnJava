f8ee0d70d1921b600822fbf7a1e28a1257fafd76
Xai <xlao@menards.net>
2016-02-12 14:15:19 -0600

    removing web wiki

--- web/wiki/js/index.js
+++
@@ -1,9 +0,0 @@
-//it will load the common as it has the common things that should be loaded on every page
-require(['./common'], function (common) {
-	console.log("common loaded");
-	//after common is loaded, it will run mainIndex.js
-	require(['app/indexContents']);
-});
-
-
-
